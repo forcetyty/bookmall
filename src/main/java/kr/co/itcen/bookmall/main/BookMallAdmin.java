4a427ec5a7ecf8998b44461a0420d4b14654d641
Tae Y. Ahn <forcetyty@gmail.com>
2019-11-14 23:52:28 +0900

    Admin - 회원정보 출력 기능 구현

--- src/main/java/kr/co/itcen/bookmall/main/BookMallAdmin.java
+++ src/main/java/kr/co/itcen/bookmall/main/BookMallAdmin.java
@@ -1,29 +1,72 @@
 package kr.co.itcen.bookmall.main;
 
+import java.util.List;
 import java.util.Scanner;
+
+import kr.co.itcen.bookmall.dao.MemberDao;
+import kr.co.itcen.bookmall.vo.Member;
 
 // BookMall 관리자 화면!!!
 public class BookMallAdmin {
 	private String ID = "admin";
 	private String PW = "1234";
-	
+
 	private Scanner scan = new Scanner(System.in);
 	
+	/////////////////////
+	// 관리자 로그인 처리 Process
 	public void loginProcess() {
 		String id = null;
 		String password = null;
-		
-		System.out.print("ID :" );
+
+		System.out.print("ID :");
 		id = scan.next().trim();
-		System.out.print("password :" );
+		System.out.print("password :");
 		password = scan.next().trim();
-		
-		if(ID.equals(id) || PW.equals(password)) {
-			System.out.println("잘 못 입력하셨습니다.");
-		}else if(ID.equals(id) && PW.equals(password)) {
+
+		// 관리자
+		if (ID.equals(id) && PW.equals(password)) {
 			System.out.println("로그인 되었 습니다.");
+			adminProcess();
+		} else if (!ID.equals(id)) {
+			System.out.println("잘 못 입력하셨습니다. ");
+		} else if (!PW.equals(password)) {
+			System.out.println("잘 못 입력하셨습니다. ");
+		}
+	}
+	/////////////////////
+	
+	/////////////////////
+	// 로그인된 관리자 처리 Process
+	public void adminProcess() {
+		int choice = 0;
+		
+		System.out.println("1. 회원정보 출력");
+		choice = scan.nextInt();
+		
+		switch (choice) {
+		case 1:
+			memberDaoTest();
+			break;
+		default:
+			break;
 		}
 		
+		
 	}
+
+	/////////////////////
+	// 회원리스트를 출력하는 메소드
+	public void memberDaoTest() {
+		MemberDao dao = new MemberDao();
+
+		List<Member> list = dao.memberList();
+
+		for (Member member : list) {
+			System.out.println("유저 아이디 :" + member.getUserid() + " 이름 :" + member.getName() + " 핸드폰 번호 :"
+					+ member.getPnumber() + " 이메일 주소 :" + member.getEmail());
+		}
+	}
+	///////////////////////
 
 }
